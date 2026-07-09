adfb5feeaea58c204e9fbb5c7b99c845fcc21c52
AbdulrahmanReda70 <abdooreda70@gmail.com>
2026-07-09 23:17:57 +0300

    Add UserRepository; implement CreateUser method for user creation

---
+++ repository/user.go
@@ -0,0 +1,15 @@
+package repository
+
+import "task-manager-api/model"
+
+type UserRepository struct {
+	db dbConnectionInterface
+}
+
+func (r *UserRepository) CreateUser(user model.User) (model.User, error) {
+	err := r.db.QueryRow("INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id", user.Username, user.Password).Scan(&user.ID)
+	if err != nil {
+		return model.User{}, err
+	}
+	return user, nil
+}
